c66cd113ad5f64e7718c8409ab859273391e896b
amanda-delongchamp <amanda.delongchamp@gmail.com>
2021-04-19 09:36:20 -0700

    initial commit

---
+++ Variables_Exercise/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Variables_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dogName = "Betty";
+            int dogAge = 1;
+            char eyeColorLetter = 'H';
+            bool isPretty = true;
+            double dogWeight = 35.5;
+            decimal dogCost = 1300.35m;
+
+            Console.WriteLine($"My dog's name is {dogName}, and she is {dogAge} old.  {dogName} has {eyeColorLetter} color of eyes, she weights {dogWeight} and her original cost was {dogCost}");
+
+
+        }
+    }
+}
